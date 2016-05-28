8b5301444f51ed4384a624fc99bbb95748e1248d
Felipe Minello <felipeminello@gmail.com>
2016-05-28 19:47:12 -0300

    Update e Delete

--- module/Application/src/Application/Service/Categoria.php
+++ module/Application/src/Application/Service/Categoria.php
@@ -32,4 +32,26 @@ class Categoria
 
 		return $categoriaEntity;
 	}
+
+	public function update(array $data)
+	{
+		$categoriaEntity = $this->entityManager->getReference('Application\Entity\Categoria', $data['id']);
+
+		$categoriaEntity->setNome($data['nome']);
+
+		$this->entityManager->persist($categoriaEntity);
+		$this->entityManager->flush();
+
+		return $categoriaEntity;
+	}
+
+	public function delete($id)
+	{
+		$categoriaEntity = $this->entityManager->getReference('Application\Entity\Categoria', $id);
+
+		$this->entityManager->remove($categoriaEntity);
+		$this->entityManager->flush();
+
+		return $id;
+	}
 }
