cd51be7c1ccc9dee9d01772ebf752f49ef6e8bb7
Felipe Minello <felipeminello@gmail.com>
2016-05-28 21:18:32 -0300

    Configurando doctrine

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace User;
+
+return array(
+	'router' => array(
+		'routes' => array(
+			'user-auth' => array(
+				
+			)
+		)
+	),
+	'doctrine' => array(
+		'driver' => array(
+			__NAMESPACE__ . '_driver' => array(
+				'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+				'cache' => 'array',
+				'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+			),
+			'orm_default' => array(
+				'drivers' => array(
+					__NAMESPACE__.'\Entity' => __NAMESPACE__ . '_driver'
+				)
+			)
+		)
+	)
+);
